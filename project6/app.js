c4f92f2ed94c7be96b90de4894f4e242b5640ffb
ksim02-SU <kyuhos1999@gmail.com>
2023-12-15 01:52:04 -0500

    .

--- project6/app.js
+++ project6/app.js
@@ -1,34 +1,71 @@
-const intro = document.querySelector(".intro");
-const video = intro.querySelector("video");
-const text = intro.querySelector("h1");
-//END SECTION
+if (window.innerWidth > 700)
+{
+    const intro = document.querySelector(".intro");
+    const video = intro.querySelector("video");
 
 
-//SCROLLMAGIC
-const controller = new ScrollMagic.Controller();
+    const controller = new ScrollMagic.Controller();
 
-//Scenes
-let scene = new ScrollMagic.Scene({
-  duration: 14000,
-  triggerElement: intro,
-  triggerHook: 0
-})
-  .addIndicators()
-  .setPin(intro)
-  .addTo(controller);
 
-//Video Animation
-let accelamount = 0.1;
-let scrollpos = 0;
-let delay = 0;
+    let scene = new ScrollMagic.Scene({
+    duration: 14000,
+    triggerElement: intro,
+    triggerHook: 0
+    })
 
-scene.on("update", e => {
-  scrollpos = e.scrollPos / 1000;
-});
+    .setPin(intro)
+    .addTo(controller);
 
-setInterval(() => {
-  delay += (scrollpos - delay) * accelamount;
-  console.log(scrollpos, delay);
+    let accelamount = 0.1;
+    let scrollpos = 0;
+    let delay = 0;
 
-  video.currentTime = delay;
-}, 33.3);
+    scene.on("update", e => {
+    scrollpos = e.scrollPos / 1000;
+    });
+
+    setInterval(() => {
+    delay += (scrollpos - delay) * accelamount;
+
+
+    video.currentTime = delay;
+    }, 33.3);
+}
+else {
+    const intro = document.querySelector(".intro2");
+    const video = intro.querySelector("video");
+    const controller = new ScrollMagic.Controller();
+
+    let scene = new ScrollMagic.Scene({
+    duration: 11000,
+    triggerElement: intro,
+    triggerHook: 0
+    })
+
+    .setPin(intro)
+    .addTo(controller);
+
+    let accelamount = 0.1;
+    let scrollpos = 0;
+    let delay = 0;
+
+    scene.on("update", e => {
+    scrollpos = e.scrollPos / 1000;
+    });
+
+    setInterval(() => {
+    delay += (scrollpos - delay) * accelamount;
+    console.log(scrollpos, delay);
+
+    video.currentTime = delay;
+    }, 33.3);
+}
+        document.addEventListener('DOMContentLoaded', () => {
+        const hamburger = document.getElementById('hamburger');
+        const navlinkid = document.getElementById('navlinkid');
+
+        hamburger.addEventListener('click', () => {
+            navlinkid.style.display = navlinkid.style.display === 'block' ? 'none' : 'block';
+        });
+    });
+
